f48c2a463128fc7b4b5e8e5d6340474e22226a8a
kigue <65794738+kigue@users.noreply.github.com>
2021-10-27 08:51:58 -0400

    html and js changes

---
+++ app/public/api/refs/refs.php
@@ -0,0 +1,29 @@
+<?php
+// require 'common.php';
+require 'class/DbConnection.php';
+
+// Step 1: Get a datase connection from our helper class
+$db = DbConnection::getConnection();
+
+// Step 2: Create & run the query
+$sql = 'SELECT * FROM referees';
+$vars = [];
+
+ // if (isset($_GET['student'])) {
+ //   // This is an example of a parameterized query
+ //   $sql = 'SELECT * FROM offer WHERE studentId = ?';
+ //   $vars = [ $_GET['student'] ];
+ // }
+
+$stmt = $db->prepare($sql);
+$stmt->execute($vars);
+
+$patients = $stmt->fetchAll();
+
+// Step 3: Convert to JSON
+$json = json_encode($patients, JSON_PRETTY_PRINT);
+
+// Step 4: Output
+header('Content-Type: application/json');
+echo $json;
+    
--- app/public/js/referee_name.js
+++ app/public/js/referee_name.js
@@ -1,123 +1,78 @@
-
-const SomeApp = {
-  data() {
-    return {
-      students: [],
-      selectedStudent: null,
-      offers: [],
-      offerForm: {},
-      selectedOffer: null
-    }
-  },
-  computed: {},
-  methods: {
-      prettyData(d) {
-          return dayjs(d)
-          .format('D MMM YYYY')
-      },
-      prettyDollar(n) {
-          const d = new Intl.NumberFormat("en-US").format(n);
-          return "$ " + d;
-      },
-      selectStudent(s) {
-          if (s == this.selectedStudent) {
-              return;
-          }
-          this.selectedStudent = s;
-          this.offers = [];
-          this.fetchOfferData(this.selectedStudent);
-      },
-      fetchStudentData() {
-          fetch('/api/student/')
-          .then( response => response.json() )
-          .then( (responseJson) => {
-              console.log(responseJson);
-              this.students = responseJson;
-          })
-          .catch( (err) => {
-              console.error(err);
-          })
-      },
-      fetchOfferData(s) {
-          console.log("Fetching offer data for ", s);
-          fetch('/api/offer/?student=' + s.id)
-          .then( response => response.json() )
-          .then( (responseJson) => {
-              console.log(responseJson);
-              this.offers = responseJson;
-          })
-          .catch( (err) => {
-              console.error(err);
-          })
-          .catch( (error) => {
-              console.error(error);
-          });
-      },
-      postOffer(evt) {
-        if (this.selectedOffer === null) {
-            this.postNewOffer(evt);
-        } else {
-            this.postEditOffer(evt);
+const Referees = {
+    data() {
+      return {
+        "referees":[],
         }
-      },
-      postNewOffer(evt) {
-        this.offerForm.studentId = this.selectedStudent.id;
+    },
 
-        console.log("Creating!", this.offerForm);
+    computed:{},
+   
+    methods: {
+        fetchRefData() {
+            console.log("A");
+            fetch('api/refs/refs.php')
+            .then( response => response.json() )
+            .then( (responseJson) => {
+                console.log(responseJson);
+                this.referees = responseJson;
+            })
+            .catch( (err) => {
+                console.error(err);
+            })
+            console.log("B");
+        },
+    
 
-        fetch('api/offer/create.php', {
-            method:'POST',
-            body: JSON.stringify(this.offerForm),
-            headers: {
-              "Content-Type": "application/json; charset=utf-8"
-            }
-          })
-          .then( response => response.json() )
-          .then( json => {
-            console.log("Returned from post:", json);
-            // TODO: test a result was returned!
-            this.offers = json;
+      //   postNewRefs(evt) {  
+      //       console.log("Create:", this.refForm);
 
-            // reset the form
-            this.resetOfferForm();
-          });
-      },
-      postEditOffer(evt) {
-        this.offerForm.studentId = this.selectedStudent.id;
-        this.offerForm.id = this.selectedOffer.id;
+      //       fetch('api/refs/create_ref.php', {
+      //           method:'POST',
+      //           body: JSON.stringify(this.refForm),
+      //           headers: {
+      //             "Content-Type": "application/json; charset=utf-8"
+      //           }
+      //         })
+      //         .then( response => response.json() )
+      //         .then( json => {
+      //           console.log("Returned from post:", json);
+      //           // TODO: test a result was returned!
+      //           this.refs = json;
+      //           this.refForm ={};
+      //         });
+      // }
 
-        console.log("Updating!", this.offerForm);
+      //   postDeleteBook(evt) {  
+      //       if (!confirm("Are you sure you want to delete the book"))        
+                
 
-        fetch('api/offer/update.php', {
-            method:'POST',
-            body: JSON.stringify(this.offerForm),
-            headers: {
-              "Content-Type": "application/json; charset=utf-8"
-            }
-          })
-          .then( response => response.json() )
-          .then( json => {
-            console.log("Returned from post:", json);
-            // TODO: test a result was returned!
-            this.offers = json;
+      //       fetch('api/books/delete.php', {
+      //           method:'POST',
+      //           body: JSON.stringify(this.bookForm),
+      //           headers: {
+      //             "Content-Type": "application/json; charset=utf-8"
+      //           }
+      //         })
+      //         .then( response => response.json() )
+      //         .then( json => {
+      //           console.log("Returned from post:", json);
+      //           // TODO: test a result was returned!
+      //           this.books = json;
+      //           this.bookForm ={};
+      //     });
+      // }
+      
+      
+    },
 
-            // reset the form
-            this.resetOfferForm();
-          });
-      },
-      selectOfferToEdit(o) {
-          this.selectedOffer = o;
-          this.offerForm = Object.assign({}, this.selectedOffer);
-      },
-      resetOfferForm() {
-          this.selectedOffer = null;
-          this.offerForm = {};
-      }
-  },
-  created() {
-      this.fetchStudentData();
-  }
 
-}
+    
 
-Vue.createApp(SomeApp).mount('#offerApp');
+
+        
+    created() {
+        this.fetchRefData();
+    } //end created
+} // end Offer config
+  
+Vue.createApp(Referees).mount('#refsApp');
